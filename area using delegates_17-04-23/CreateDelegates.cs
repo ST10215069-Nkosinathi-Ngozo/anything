78415a3746fa9749945a534035c89845f267a7f2
ST10215069_Nkosinathi Ngozo <104794600+ST10215069-Nkosinathi-Ngozo@users.noreply.github.com>
2023-04-24 15:13:44 +0200

    Add files via upload

---
+++ area using delegates_17-04-23/CreateDelegates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area_using_delegates_17_04_23
+{
+    public delegate void rectangle(double x, double y);
+    public delegate void triangle(double x, double y);
+    internal class CreateDelegates
+    {
+        public double RectangleArea(double l, double b)
+        {
+            return l * b;
+        }
+
+        public double TriangleArea(double l, double b)
+        {
+            return l * b * .5;
+        }
+    }
+    
+}
---
+++ area using delegates_17-04-23/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace area_using_delegates_17_04_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle r = new rectangle(delegate(double l, double b)
+            {
+                Console.WriteLine(l * b );
+            });
+
+            triangle t = new triangle(delegate (double l, double b)
+            {
+                Console.WriteLine(l * b * .5);
+            });
+            double l = 12, b = 3;
+            r(l, b);
+            t(l, b);
+        }
+    }
+}
